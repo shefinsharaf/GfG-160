1ae04234d50ab144bfc3ac15298bfe989e8f1e05
Shefi <134257203+shefinsharaf@users.noreply.github.com>
2024-12-29 18:35:39 +0530

    Add files via upload

---
+++ zerotoend.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pushZerosToEnd(vector<int> &arr) {
+    int count =0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if(arr[i] !=0)
+         {
+            swap(arr[i],arr[count]);
+            count++;
+         }
+    }
+    
+}
+
+int main() {
+    vector<int> arr = {1, 2, 0, 4, 3, 0, 5, 0};
+    pushZerosToEnd(arr);
+    for (int num : arr)
+        cout << num << ' ';
+    return 0;
+}
