d210cf244ecb9ce8f474a35e3fd2c150fea9a3d4
Shefi <134257203+shefinsharaf@users.noreply.github.com>
2025-01-01 21:12:11 +0530

    Add files via upload

---
+++ nextpermutation.cpp
@@ -0,0 +1,7 @@
+class Solution {
+  public:
+    void nextPermutation(vector<int>& arr) {
+        //in c++ there is a built in function for finding the next permutation directly
+    next_permutation(arr.begin(),arr.end());
+    }
+};
